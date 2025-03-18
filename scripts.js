81ab28f67eac29c4f0e08b651ba013f34990b708
Moritz Richter <38633505+y0091797@users.noreply.github.com>
2025-03-18 22:35:34 +0100

    Long Awaited Update
    Changed Color Theme. And many more updates to the website.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,35 @@
 let slideIndex = 0;
-showSlides();
+showSlides(slideIndex);
 
-function showSlides() {
+// Funktion für Vor- oder Zurückgehen
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    
+    if (n >= slides.length) {
+        slideIndex = 0;  
+    }
+    if (n < 0) {
+        slideIndex = slides.length - 1;
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].classList.remove("active");
+    }
+    
+    slides[slideIndex].style.display = "block";  
+    dots[slideIndex].classList.add("active");
+}
+
+/*function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("mySlides");
     let dots = document.getElementsByClassName("dot");
@@ -22,4 +50,6 @@ function showSlides() {
     dots[slideIndex-1].className += " active";
     
     setTimeout(showSlides, 5000); // Ändere hier die Zeit in Millisekunden für den Bildwechsel
-}
+}*/
+
+
